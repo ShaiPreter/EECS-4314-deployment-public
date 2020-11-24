ca842c25e73ae80225d1e486d4952f698f96032b
ShaiPreter <shai.preter@gmail.com>
2020-11-23 22:46:35 -0500

    avoided duplicate ports

--- app.js
+++ app.js
@@ -56,6 +56,6 @@ app.use('/clubs',require('./routes/clubs'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server started on port ${PORT}`));
+//app.listen(PORT, console.log(`Server started on port ${PORT}`));
 
 module.exports = app

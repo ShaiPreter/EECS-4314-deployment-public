342b7ceedd3d048c0a59a044e942cffa2d08a53a
ShaiPreter <shai.preter@gmail.com>
2020-12-01 22:04:02 -0500

    added message when user exceeds limit

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ const rateLimit = require("express-rate-limit");
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 10, // limit each IP to 100 requests per windowMs
-    message: 'You have exceeded the 100 requests in 24 hrs limit!',
+    message: 'You have exceeded the 100 requests in 15 minutes limit!',
     headers: true,
 });
 
